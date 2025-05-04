c14c94cb99ab936f29eb49b98a73e4f5ce88b937
Ivan <itverdyy@gmail.com>
2025-05-04 21:29:40 +0300

    finished async client, need to show performance increase

--- core/async_client.py
+++ core/async_client.py
@@ -11,13 +11,29 @@ def single_request(url):
     return resp, end_time - start_time
 
 def async_client(n=20, url='http://0.0.0.0:5000/delay'):
-    with ThreadPoolExecutor(max_workers=n) as executor:
+    t = time.time()
+    with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(single_request, url) for _ in range(n)]
 
-
+        res = []
+        times = []
         for i in futures:
             r, duration = i.result()
-            print(r, duration)
+            res.append(r['delay'])
+            times.append(duration)
+    end = time.time()
+    async_time = end - t
+    return res, times, async_time
 
 
-async_client()
+if __name__ == '__main__':
+    n = 20
+    res, times, async_time = async_client(n=n, url='http://0.0.0.0:5000/delay')
+    print("=" * 30)
+    print(times)
+    print(res)
+    print(async_time)
+
+    result = {"SyncResp": res, "SyncRes": times}
+    with open("../results/result_async_threads.json", "w") as f:
+        json.dump(result, f)
--- core/sync_client.py
+++ core/sync_client.py
@@ -4,20 +4,21 @@ import json
 
 def sync_client(n, url='http://0.0.0.0:5000/delay'):
     num_of_times = []
+    resp = []
     for _ in range(n):
         start = time.time()
         raw_resp = requests.get(url)
-        resp = json.loads(raw_resp.text)
-        print(resp)
+        resp.append(json.loads(raw_resp.text)['delay'])
         end = time.time()
         num_of_times.append(end - start)
-    return num_of_times
+    return resp, num_of_times
 
 if __name__ == '__main__':
     n = 20
-    times = sync_client(n)
-    print("="*30)
+    resp, times = sync_client(n)
+    print("=" * 30)
     print(times)
-    res = {"SyncRes": times}
+    print(resp)
+    res = {"SyncResp": resp, "SyncRes": times}
     with open("../results/result_sync.json", "w") as f:
         json.dump(res, f)
